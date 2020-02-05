78fc980ecede1a44190f096d8c3556fc060ae25e
8977shiva <b.skreddy1997@gmail.com>
2020-02-05 08:48:04 -0800

    homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import './App.css';
-import HomePage from './components/HomePage/HomePage';
+import './App.css'
+import HomePage from './pages/HomePage/HomePage';
 
 function App() {
   return (
