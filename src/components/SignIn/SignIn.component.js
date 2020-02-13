1266a5f12f84e35c0363cfef6a6178585eec4601
8977shiva <b.skreddy1997@gmail.com>
2020-02-13 09:24:13 -0800

    custombutton

--- src/components/SignIn/SignIn.component.js
+++ src/components/SignIn/SignIn.component.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./SignIn.styles.scss";
- import FormInput from '../customInput/formInput.component'
+import FormInput from "../customInput/formInput.component";
+import CustomButton from "../customButton/customButtom.component";
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
@@ -12,13 +13,12 @@ class SignIn extends React.Component {
   handleSubmit = event => {
     event.preventDefualt();
     this.setState({ email: "", password: "" });
-    console.log('f')
+    console.log("f");
   };
-  handleChange=(event)=>{
-     this.setState({[event.target.name]: event.target.value})  
-      console.log("fs")
-
-  }
+  handleChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+    console.log("fs");
+  };
   render() {
     const { email, password } = this.state;
     return (
@@ -26,10 +26,23 @@ class SignIn extends React.Component {
         <h2>I already have an account</h2>
         <span>Sigin with your email and password</span>
         <form onSubmit={this.handleSubmit}>
-          <FormInput name="Email" type="email" value={email}  handleChange={this.handleChange} required label="Email"/>
-          <FormInput name="password" type="password" value={password} handleChange={this.handleChange} label="Password"/>
+          <FormInput
+            name="email"
+            type="email"
+            value={email}
+            handleChange={this.handleChange}
+            required
+            label="Email"
+          />
+          <FormInput
+            name="password"
+            type="password"
+            value={password}
+            handleChange={this.handleChange}
+            label="Password"
+          />
 
-          <input type="submit" value="submit form" />
+          <CustomButton type="submit">SignIn </CustomButton>
         </form>
       </div>
     );
