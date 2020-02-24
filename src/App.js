39f1c71da3f06b02120cd70421593abca4d567d3
8977shiva <b.skreddy1997@gmail.com>
2020-02-24 15:17:55 -0800

    auth 2d

--- src/App.js
+++ src/App.js
@@ -27,17 +27,22 @@ class App extends React.Component {
 
    }
  }
+ unsubcribeFormAuth=null;
   componentDidMount(){
-    auth.onAuthStateChanged(user=>{
+    this.unsubcribeFormAuth=auth.onAuthStateChanged(user=>{
       this.setState({currentUser:user})
       console.log(user)
     })
   }
+   componentWillUnmount(){
+     this.unsubcribeFormAuth();
+     console.log('d')
+   }
 
   render(){
   return (
   <div>
-    <HeaderComponent/>
+    <HeaderComponent currentUser={this.state.currentUser}/>
     <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route  path='/shop' component={ShopPage}/>
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,21 +1,30 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import {ReactComponent as Logo} from '../../assests/crown.svg'
-import './header.style.scss'
-const HeaderComponent=()=>(
-<div className='header'>
-    <Link className='logo-container' to='/'>
-      <Logo className='logo' />
+import React from "react";
+import { Link } from "react-router-dom";
+import { ReactComponent as Logo } from "../../assests/crown.svg";
+import "./header.style.scss";
+import { auth } from "../../firebase/firebase.utli";
+const HeaderComponent = ({ currentUser }) => (
+  <div className="header">
+    <Link className="logo-container" to="/">
+      <Logo className="logo" />
     </Link>
-    <div className='options'>
-      <Link className='option' to='/shop'>
+    <div className="options">
+      <Link className="option" to="/shop">
         SHOP
       </Link>
-      <Link className='option' to='/shop'>
+      <Link className="option" to="/shop">
         CONTACT
       </Link>
-      <Link  className='option' to='/signin'>SIGNIN</Link>
+      {currentUser ? (
+        <div className="option" onClick={() => auth.signOut()}>
+          SIGNOUT
+        </div>
+      ) : (
+        <Link className="option" to="/signin">
+          SIGNIN
+        </Link>
+      )}
     </div>
   </div>
-)
-export default HeaderComponent
+);
+export default HeaderComponent;
