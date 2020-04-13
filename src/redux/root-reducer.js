f6d04e86aeb0126d466f7d8d2b350887a110cfc6
8977shiva <b.skreddy1997@gmail.com>
2020-04-13 23:50:06 +0530

    'updates'

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,6 +1,8 @@
-import{ combineReducers} from 'redux';
-import userReducer from './user/user.reducer';
- 
+import { combineReducers } from "redux";
+import userReducer from "./user/user.reducer";
+
 export default combineReducers({
-    user:userReducer
-})
+  user: userReducer
+});
+
+
