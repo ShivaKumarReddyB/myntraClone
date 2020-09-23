f0dbd5be547c00bf7db1757e568919c46a367e5a
8977shiva <b.skreddy1997@gmail.com>
2020-09-23 09:59:47 -0700

    redirect when user signin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Switch, Route } from "react-router-dom";
-import {connect }from 'react-redux';
+import { Switch, Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
 
 import "./App.css";
 import HomePage from "./pages/HomePage/HomePage";
@@ -8,8 +8,7 @@ import ShopPage from "./pages/Shop/ShopPage";
 import HeaderComponent from "./components/header/header";
 import SigninAndSignOut from "./pages/SigninAndSignout/SigninAndSignout";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utli";
-import {setCurrentUser} from './redux/user/user.action'
-
+import { setCurrentUser } from "./redux/user/user.action";
 
 const HatsPage = () => (
   <div>
@@ -18,23 +17,22 @@ const HatsPage = () => (
 );
 
 class App extends React.Component {
-
   unsubcribeFormAuth = null;
   componentDidMount() {
-    const { setCurrentUser}=this.props;
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    const { setCurrentUser } = this.props;
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot => {
+        userRef.onSnapshot((snapShot) => {
           setCurrentUser({
             id: snapShot.id,
-            ...snapShot.data()
+            ...snapShot.data(),
           });
         });
       }
 
-     setCurrentUser(userAuth);
+      setCurrentUser(userAuth);
     });
   }
   componentWillUnmount() {
@@ -44,19 +42,31 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <HeaderComponent  />
+        <HeaderComponent />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
-          <Route path="/signin" component={SigninAndSignOut} />
+          <Route
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? (
+                <Redirect to="/" />
+              ) : (
+                <SigninAndSignOut />
+              )
+            }
+          />
           <Route path="/hats" component={HatsPage} />
         </Switch>
       </div>
     );
   }
 }
-
-const mapDispatchToProps = dispatch => ({
-  setCurrentUser: user => dispatch(setCurrentUser(user))
+const mapStateToPorps = ({ user }) => ({
+  currentUser: user.currentUser,
 });
-export default  connect(null,mapDispatchToProps)(App);
+
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+export default connect(mapStateToPorps, mapDispatchToProps)(App);
--- src/redux/user/user.reducer.js
+++ src/redux/user/user.reducer.js
@@ -1,17 +1,17 @@
-const INITIAL_STATE={
-    currentUser:null
-}
+import { UserActionTypes } from "./user.types";
+const INITIAL_STATE = {
+  currentUser: null,
+};
 
-
-const userReducer=(state=INITIAL_STATE,action)=>{
-    switch(action.type){
-        case 'SET_CURRENT_USER':
-            return{
-                ...state,
-                currentUser:action.payload
-            }
-        default:
-            return state;
-    }
-}
+const userReducer = (state = INITIAL_STATE, action) => {
+  switch (action.type) {
+    case UserActionTypes.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: action.payload,
+      };
+    default:
+      return state;
+  }
+};
 export default userReducer;
